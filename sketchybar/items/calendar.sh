bacad626c9029d400edad626e183319f2cba8052
Felix <85020353+Falo04@users.noreply.github.com>
2025-03-03 09:08:25 +0100

    Delete sketchybar directory

--- sketchybar/items/calendar.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-sketchybar --add item calendar right \
-           --set calendar icon=􀧞  \
-                          update_freq=30 \
-                          icon.color=$WHITE \
-                          label.color=$WHITE \
-                          background.color=$ACCENT_COLOR\
-                          script="$PLUGIN_DIR/calendar.sh"
