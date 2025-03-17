9217e3f79e03960fde35511601e80a1438c9e768
Falo04 <felixwallner0@gmail.com>
2025-03-17 19:56:43 +0100

    readded karabiner and added templates to nvim

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -2,3 +2,5 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 vim.keymap.set("n", "<leader>k", vim.diagnostic.open_float, { noremap = true, silent = true })
+
+vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
--- nvim/snippets/typescriptreact.lua
+++ nvim/snippets/typescriptreact.lua
@@ -1,11 +1,11 @@
 local ls = require("luasnip")
 local s = ls.snippet
 local t = ls.text_node
-local i = ls.insert_node
 
 ls.add_snippets("typescriptreact", {
   s("rts", {
-    t({ "import { createFileRoute } from '@tanstack/react-router';", "", "" }),
+    t({ "import { createFileRoute } from '@tanstack/react-router';", "" }),
+    t({ "import { useTranslation } from 'react-i18next';", "", "" }),
     t({ "/**", "" }),
     t({ "  * The properties for {@link CompName}", "" }),
     t({ "  */", "" }),
@@ -16,6 +16,7 @@ ls.add_snippets("typescriptreact", {
     t({ "  * The CompName", "" }),
     t({ "  */", "" }),
     t({ "function CompName(props: CompNameProps) {", "" }),
+    t({ "    const [t] = useTranslation();", "", "" }),
     t({ "    return (", "" }),
     t({ "         <div></div>", "" }),
     t({ "    );", "" }),
