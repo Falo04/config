06251d47e890f0e55d519a255a5883014a79b0d9
Falo04 <felixwallner0@gmail.com>
2025-06-29 11:24:07 +0200

    added some abbr function and added tide as fish prompt

--- fish/config.fish
+++ fish/config.fish
@@ -1,3 +1,8 @@
+abbr --add up python3 tools.py up -d
+abbr --add build python3 tools.py up -d --build
+abbr --add down python3 tools.py down
+abbr --add logs python3 tools.py logs -tf
+
 zoxide init fish | source
 
 set -gx GPG_TTY (tty)
